76e4c0588da758c9e6fceae0fc806ebe5a8ac454
rlova <lovaremano@gmail.com>
2025-09-24 17:45:39 +0200

    ajout jeudecartes

---
+++ src/cartes/JeuDeCartes.java
@@ -0,0 +1,17 @@
+package cartes;
+
+public class JeuDeCartes {
+	
+	private static class Configuration {
+		private Carte carte;
+		private int nbExemplaire;
+		
+		public Configuration(Carte carte, int nbExemplaire) {
+			super();
+			this.carte = carte;
+			this.nbExemplaire = nbExemplaire;
+		}
+		
+	}
+	
+}
