ff7acb55f5330e52c5a6b080aa1c7bfb0a91775f
rlova <lovaremano@gmail.com>
2025-09-27 21:15:06 +0200

    maj jeuDeCartes

--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -1,7 +1,18 @@
 package cartes;
 
 public class JeuDeCartes {
+	private Configuration[] configurations = {
+			new Configuration(new Borne(25), 10),
+			new Configuration(new Borne(50), 10),
+			new Configuration(new Borne(75), 10),
+			new Configuration(new Borne(100), 15),
+			new Configuration(new Borne(200), 4),
+			new Configuration(new Attaque(Type.FEU), 14),	// Feu Vert
+			new Configuration(new FinLimite(), 6),	// Fin Limite
+			new Configuration(new Parade(Type.ESSENCE), 0)	// Bidon d'essence
+	};
 	
+	// classe interne
 	private static class Configuration {
 		private Carte carte;
 		private int nbExemplaire;
@@ -11,6 +22,15 @@ public class JeuDeCartes {
 			this.carte = carte;
 			this.nbExemplaire = nbExemplaire;
 		}
+
+		public Carte getCarte() {
+			return carte;
+		}
+
+		public int getNbExemplaire() {
+			return nbExemplaire;
+		}
+		
 		
 	}
 	
